e7b72dd65f83fc9d5f7872a0abca4283d48eba5b
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-10-12 01:29:34 +0530

    Update PermissionController.java
    added few endpoints

--- UserService/app/src/main/java/com/bookmyshow/userservice/controller/PermissionController.java
+++ UserService/app/src/main/java/com/bookmyshow/userservice/controller/PermissionController.java
@@ -1,8 +1,37 @@
 package com.bookmyshow.userservice.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import com.bookmyshow.userservice.dao.entity.Permission;
+import com.bookmyshow.userservice.dto.PermissionDTO;
+import com.bookmyshow.userservice.dto.RoleDTO;
+import com.bookmyshow.userservice.service.PermissionService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
 
 @RestController
+@RequestMapping(path = "/api/v1")
 public class PermissionController {
+
+    @Autowired
+    private PermissionService service;
+
+    @PostMapping(path = "/permissions")
+    public ResponseEntity<PermissionDTO> addPermission(@Valid @RequestBody PermissionDTO permissionDTO){
+        return new ResponseEntity( service.addPermission(permissionDTO) , HttpStatus.CREATED);
+    }
+
+    @GetMapping(path = "/permissions")
+    public ResponseEntity<List<PermissionDTO>> getAllPermissions() {
+        return new ResponseEntity<>( service.getAllPermissions() , HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/permissions/{id}")
+    public ResponseEntity<PermissionDTO> getAllPermissions(@PathVariable Long id) {
+        return new ResponseEntity<>( service.getPermissionById(id) , HttpStatus.OK);
+    }
 
 }
