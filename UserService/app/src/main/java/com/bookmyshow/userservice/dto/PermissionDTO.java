165e0ad44823a9402d33e2a81b54ef8b64eb8a48
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-10-12 01:28:55 +0530

    Create PermissionDTO.java

---
+++ UserService/app/src/main/java/com/bookmyshow/userservice/dto/PermissionDTO.java
@@ -0,0 +1,73 @@
+package com.bookmyshow.userservice.dto;
+
+import com.bookmyshow.userservice.dao.entity.Role;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+import javax.validation.constraints.NotBlank;
+import java.util.Set;
+
+@JsonPropertyOrder({
+        "id",
+        "permissionCode",
+        "permissionDescription"
+})
+public class PermissionDTO {
+
+    private Long id;
+
+    @NotBlank
+    private String permissionCode;
+
+    @NotBlank
+    private String permissionDescription;
+
+    @JsonIgnore
+    private Set<Role> roles;
+
+    public PermissionDTO() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "PermissionDTO{" +
+                "id=" + id +
+                ", permissionCode='" + permissionCode + '\'' +
+                ", permissionDescription='" + permissionDescription + '\'' +
+                ", roles=" + roles +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPermissionCode() {
+        return permissionCode;
+    }
+
+    public void setPermissionCode(String permissionCode) {
+        this.permissionCode = permissionCode;
+    }
+
+    public String getPermissionDescription() {
+        return permissionDescription;
+    }
+
+    public void setPermissionDescription(String permissionDescription) {
+        this.permissionDescription = permissionDescription;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
+}
