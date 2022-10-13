ebee00cf57f4e5914d83de0e6bba35703da48d3c
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-10-14 01:52:27 +0530

    Create UserResponseDTO.java

---
+++ UserService/app/src/main/java/com/bookmyshow/userservice/dto/UserResponseDTO.java
@@ -0,0 +1,77 @@
+package com.bookmyshow.userservice.dto;
+
+import java.util.Date;
+
+public class UserResponseDTO {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Long roleId;
+    private Date createdAt;
+
+
+    public UserResponseDTO() {
+    }
+
+    @Override
+    public String toString() {
+        return "UserResponseDTO{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", roleId=" + roleId +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(Long roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+}
