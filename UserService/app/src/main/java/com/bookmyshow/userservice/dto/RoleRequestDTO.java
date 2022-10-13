828f3b82e5c682396e2d1b6896c36bbf67486301
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-10-13 22:36:07 +0530

    Create RoleRequestDTO.java

---
+++ UserService/app/src/main/java/com/bookmyshow/userservice/dto/RoleRequestDTO.java
@@ -0,0 +1,41 @@
+package com.bookmyshow.userservice.dto;
+
+import javax.validation.constraints.NotBlank;
+
+public class RoleRequestDTO {
+
+    @NotBlank(message = "Role must not be empty")
+    private String role;
+
+    private String roleDescription;
+
+    public RoleRequestDTO(String role, String roleDescription) {
+        this.role = role;
+        this.roleDescription = roleDescription;
+    }
+
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRoleDescription() {
+        return roleDescription;
+    }
+
+    public void setRoleDescription(String roleDescription) {
+        this.roleDescription = roleDescription;
+    }
+
+    @Override
+    public String toString() {
+        return "RoleRequestDTO{" +
+                "role='" + role + '\'' +
+                ", roleDescription='" + roleDescription + '\'' +
+                '}';
+    }
+}
