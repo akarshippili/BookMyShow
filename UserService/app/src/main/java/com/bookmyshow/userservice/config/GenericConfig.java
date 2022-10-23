ff89ea7e9b2d25193c3b50a604d9af70ab6c5b78
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-10-24 00:59:59 +0530

    Update GenericConfig.java

--- UserService/app/src/main/java/com/bookmyshow/userservice/config/GenericConfig.java
+++ UserService/app/src/main/java/com/bookmyshow/userservice/config/GenericConfig.java
@@ -1,11 +1,11 @@
-package com.bookmyshow.userservice;
+package com.bookmyshow.userservice.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AppConfig {
+public class GenericConfig {
 
     @Bean
     public ModelMapper modelMapper(){
