677a0b42c0ef5901ad90f618eb42fd355d8b62aa
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-10-26 01:00:26 +0530

    Update RoleRepository.java

--- UserService/app/src/main/java/com/bookmyshow/userservice/dao/repo/RoleRepository.java
+++ UserService/app/src/main/java/com/bookmyshow/userservice/dao/repo/RoleRepository.java
@@ -2,9 +2,5 @@ package com.bookmyshow.userservice.dao.repo;
 
 import com.bookmyshow.userservice.dao.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> { }
