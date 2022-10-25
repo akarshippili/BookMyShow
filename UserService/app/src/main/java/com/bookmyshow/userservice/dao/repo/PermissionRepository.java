0e204ad5798dd7578dff7c01b06709b9be01ac96
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-10-26 01:00:36 +0530

    Update PermissionRepository.java

--- UserService/app/src/main/java/com/bookmyshow/userservice/dao/repo/PermissionRepository.java
+++ UserService/app/src/main/java/com/bookmyshow/userservice/dao/repo/PermissionRepository.java
@@ -2,9 +2,5 @@ package com.bookmyshow.userservice.dao.repo;
 
 import com.bookmyshow.userservice.dao.entity.Permission;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-@Repository
 public interface PermissionRepository extends JpaRepository<Permission, Long> { }
