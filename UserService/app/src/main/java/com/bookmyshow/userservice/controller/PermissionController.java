01d781c5277ad228795d4456809faab7adffd49b
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-10-27 01:10:13 +0530

    added swagger metadata

--- UserService/app/src/main/java/com/bookmyshow/userservice/controller/PermissionController.java
+++ UserService/app/src/main/java/com/bookmyshow/userservice/controller/PermissionController.java
@@ -3,6 +3,8 @@ package com.bookmyshow.userservice.controller;
 import com.bookmyshow.userservice.dto.PermissionRequestDTO;
 import com.bookmyshow.userservice.dto.PermissionResponseDTO;
 import com.bookmyshow.userservice.service.PermissionService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,27 +20,57 @@ public class PermissionController {
     @Autowired
     private PermissionService service;
 
+    @Tag(name = "Permissions")
+    @Operation(
+            method = "POST",
+            summary = "Create a new  permission",
+            description = "Create a new permission"
+    )
     @PostMapping(path = "/permissions")
     public ResponseEntity<PermissionResponseDTO> addPermission(@Valid @RequestBody PermissionRequestDTO permissionRequestDTO){
         return new ResponseEntity( service.save(permissionRequestDTO) , HttpStatus.CREATED);
     }
 
+    @Tag(name = "Permissions")
+    @Operation(
+            method = "GET",
+            summary = "Get all permissions",
+            description = "Get all permissions"
+    )
     @GetMapping(path = "/permissions")
     public ResponseEntity<List<PermissionResponseDTO>> getAllPermissions() {
         return new ResponseEntity<>( service.findAll() , HttpStatus.OK);
     }
 
+    @Tag(name = "Permissions")
+    @Operation(
+            method = "GET",
+            summary = "Get permission by id",
+            description = "Get permission by id"
+    )
     @GetMapping(path = "/permissions/{id}")
     public ResponseEntity<PermissionResponseDTO> getAllPermissions(@PathVariable Long id) {
         return new ResponseEntity<>( service.findById(id) , HttpStatus.OK);
     }
 
+    @Tag(name = "Permissions")
+    @Operation(
+            method = "PUT",
+            summary = "Update permission by id",
+            description = "Update permission by id"
+    )
     @PutMapping(path = "/permissions/{id}")
     public ResponseEntity<PermissionResponseDTO> update(@PathVariable Long id, @Valid @RequestBody PermissionRequestDTO permissionRequestDTO){
         PermissionResponseDTO updatedPermission = service.update(id, permissionRequestDTO);
         return new ResponseEntity<>(updatedPermission, HttpStatus.OK);
     }
 
+    @Tag(name = "Permissions")
+    @Operation(
+            method = "DELETE",
+            summary = "Delete permission by id",
+            description = "Delete permission by id"
+    )
     @DeleteMapping(path = "/permissions/{id}")
     public ResponseEntity<Object> delete(@PathVariable Long id){
         service.delete(id);
--- UserService/app/src/main/java/com/bookmyshow/userservice/controller/RoleController.java
+++ UserService/app/src/main/java/com/bookmyshow/userservice/controller/RoleController.java
@@ -4,6 +4,8 @@ import com.bookmyshow.userservice.dto.PermissionResponseDTO;
 import com.bookmyshow.userservice.dto.RoleRequestDTO;
 import com.bookmyshow.userservice.dto.RoleResponseDTO;
 import com.bookmyshow.userservice.service.RoleService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,44 +21,92 @@ public class RoleController {
     @Autowired
     private RoleService service;
 
+    @Tag(name = "Roles")
+    @Operation(
+            method = "POST",
+            summary = "Create a new role",
+            description = "Create a new role"
+    )
     @PostMapping(path = "/roles")
     public ResponseEntity<RoleResponseDTO> addRole(@Valid @RequestBody RoleRequestDTO role){
         return new ResponseEntity(service.save(role), HttpStatus.CREATED);
     }
 
+    @Tag(name = "Roles")
+    @Operation(
+            method = "GET",
+            summary = "Get role by id",
+            description = "Get role by id"
+    )
     @GetMapping(path = "/roles/{id}")
     public ResponseEntity<RoleResponseDTO> getRole(@PathVariable Long id) {
         return new ResponseEntity<>(service.findById(id), HttpStatus.OK);
     }
 
+    @Tag(name = "Roles")
+    @Operation(
+            method = "GET",
+            summary = "Get all permissions of a role by id",
+            description = "Get all permissions of a role by id"
+    )
     @GetMapping(path = "/roles/{id}/permissions")
     public ResponseEntity<List<PermissionResponseDTO>> getPermissionOfRole(@PathVariable Long id) {
         return new ResponseEntity<>(service.findPermissionsById(id), HttpStatus.OK);
     }
 
+    @Tag(name = "Roles")
+    @Operation(
+            method = "POST",
+            summary = "add permissions to a role by id",
+            description = "add permissions to a role by id"
+    )
     @PostMapping(path = "/roles/{id}/permissions")
     public ResponseEntity<Object> addPermissionsToRole(@PathVariable Long id, @RequestBody List<Long> permissionIds) {
         service.addPermissionsToRole(id, permissionIds);
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    @Tag(name = "Roles")
+    @Operation(
+            method = "DELETE",
+            summary = "delete permissions to a role by id",
+            description = "delete permissions to a role by id"
+    )
     @DeleteMapping(path = "/roles/{id}/permissions")
     public ResponseEntity<Object> deletePermissionToRole(@PathVariable Long id, @RequestBody List<Long> permissionIds) {
         service.deletePermissionToRole(id, permissionIds);
         return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
+    @Tag(name = "Roles")
+    @Operation(
+            method = "GET",
+            summary = "Get all roles",
+            description = "Get all roles"
+    )
     @GetMapping(path = "/roles")
     public ResponseEntity<List<RoleResponseDTO>> getAllRoles() {
         return new ResponseEntity<>(service.findAll(), HttpStatus.OK);
     }
 
+    @Tag(name = "Roles")
+    @Operation(
+            method = "PUT",
+            summary = "Update role by id",
+            description = "Update role by id"
+    )
     @PutMapping(path = "/roles/{id}")
     public ResponseEntity<RoleResponseDTO> update(@PathVariable Long id, @RequestBody RoleRequestDTO roleRequestDTO){
         RoleResponseDTO updatedRole = service.update(id, roleRequestDTO);
         return new ResponseEntity<>(updatedRole, HttpStatus.OK);
     }
 
+    @Tag(name = "Roles")
+    @Operation(
+            method = "DELETE",
+            summary = "delete role by id",
+            description = "delete role by id"
+    )
     @DeleteMapping(path = "/roles/{id}")
     public ResponseEntity<Object> delete(@PathVariable Long id){
         service.delete(id);
