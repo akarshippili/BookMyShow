9e0a6cc45524bb7ae7099510a2b84aa061a11844
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-11-03 02:02:45 +0530

    removed unused constructors
    in exception classes

--- UserService/app/src/main/java/com/bookmyshow/userservice/exception/RoleNotFoundException.java
+++ UserService/app/src/main/java/com/bookmyshow/userservice/exception/RoleNotFoundException.java
@@ -1,9 +1,6 @@
 package com.bookmyshow.userservice.exception;
 
 public class RoleNotFoundException extends RuntimeException{
-    public RoleNotFoundException(String message) {
-        super(message);
-    }
 
     public RoleNotFoundException(Long id) {
         super(String.format("Role with id: %d not found", id));
--- UserService/app/src/main/java/com/bookmyshow/userservice/exception/UserNotFoundException.java
+++ UserService/app/src/main/java/com/bookmyshow/userservice/exception/UserNotFoundException.java
@@ -1,9 +1,6 @@
 package com.bookmyshow.userservice.exception;
 
 public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message) {
-        super(message);
-    }
 
     public UserNotFoundException(Long id) {
         super(String.format("User with id: %d not found", id));
