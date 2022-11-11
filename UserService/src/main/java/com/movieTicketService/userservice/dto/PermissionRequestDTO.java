f04f11400e6b1f952cb31e32b476e1d1cf0d9c70
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-11-11 14:15:47 +0530

    Update PermissionRequestDTO.java

--- UserService/src/main/java/com/movieTicketService/userservice/dto/PermissionRequestDTO.java
+++ UserService/src/main/java/com/movieTicketService/userservice/dto/PermissionRequestDTO.java
@@ -1,10 +1,11 @@
 package com.movieTicketService.userservice.dto;
 
-import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import java.io.Serializable;
 
+@Data
 public class PermissionRequestDTO implements Serializable {
 
     @NotBlank
@@ -12,31 +13,4 @@ public class PermissionRequestDTO implements Serializable {
 
     @NotBlank
     private String description;
-
-    public PermissionRequestDTO() {
-    }
-
-    @Override
-    public String toString() {
-        return "PermissionRequestDTO{" +
-                "code='" + code + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
-
-    public String getCode() {
-        return code;
-    }
-
-    public void setCode(String code) {
-        this.code = code;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
