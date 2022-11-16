927c29ca1849ec759b148afd2b2a023bd2723996
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-11-17 01:49:24 +0530

    Update NotFoundException.java

--- UserService/src/main/java/com/movieTicketService/userservice/exception/NotFoundException.java
+++ UserService/src/main/java/com/movieTicketService/userservice/exception/NotFoundException.java
@@ -1,9 +1,6 @@
 package com.movieTicketService.userservice.exception;
 
 public class NotFoundException extends MovieTicketException {
-    public NotFoundException() {
-    }
-
     public NotFoundException(String message) {
         super(message);
     }
