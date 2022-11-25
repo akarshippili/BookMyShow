bf838f243d9e4cfb3cd45d0bce9deebf8a3b6a75
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-11-26 02:38:00 +0530

    Update AbstractService.java

--- user-service/src/main/java/com/ticketapp/userservice/service/AbstractService.java
+++ user-service/src/main/java/com/ticketapp/userservice/service/AbstractService.java
@@ -1,10 +1,9 @@
 package com.ticketapp.userservice.service;
 
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 
 public abstract class AbstractService {
-    protected ModelMapper modelMapper;
+    protected final ModelMapper modelMapper;
 
     public AbstractService(ModelMapper modelMapper) {
         this.modelMapper = modelMapper;
