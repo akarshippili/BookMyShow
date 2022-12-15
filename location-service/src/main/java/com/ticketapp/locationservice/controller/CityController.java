66e9fba95628cd3dd31f55c817fec6f0fabea2f7
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-12-15 22:46:40 +0530

    added lombok and added log statement for test

--- location-service/src/main/java/com/ticketapp/locationservice/controller/CityController.java
+++ location-service/src/main/java/com/ticketapp/locationservice/controller/CityController.java
@@ -3,6 +3,7 @@ package com.ticketapp.locationservice.controller;
 import com.ticketapp.locationservice.dto.CityRequestDTO;
 import com.ticketapp.locationservice.dto.CityResponseDTO;
 import com.ticketapp.locationservice.service.CityService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Slf4j
 @RestController
 public class CityController extends AbstractController {
 
@@ -28,6 +30,7 @@ public class CityController extends AbstractController {
 
     @GetMapping(path = "/cities")
     public ResponseEntity<List<CityResponseDTO>> getAllStates(){
+        log.info("Request for get all cities");
         return new ResponseEntity<>(service.getAllCities(), HttpStatus.OK);
     }
 
