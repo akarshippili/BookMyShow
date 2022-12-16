a0cfabe9037e786d60475e6b3e9f33a0a6c7e7e2
IPPILIAKARSH <ippiliakarsh@gmail.com>
2022-12-17 01:58:11 +0530

    Update GenericConfig.java

--- location-service/src/main/java/com/ticketapp/locationservice/config/GenericConfig.java
+++ location-service/src/main/java/com/ticketapp/locationservice/config/GenericConfig.java
@@ -1,11 +1,11 @@
-package com.ticketapp.locationservice;
+package com.ticketapp.locationservice.config;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class AppConfig {
+public class GenericConfig {
 
     @Bean
     public ModelMapper modelMapper(){
